3a2d3df14bec4530967a7ff1f96e00da6cfb85b2
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-20 22:40:27 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -10,18 +10,15 @@ class Author
   end
 
   def add_post(new_post)
-    # Author #add_post takes in an argument of a post and associates that post with the author by telling the post that it belongs to that author
     new_post.author = self
   end
 
   def add_post_by_title(post_title)
-    # Author #add_post_by_title takes in an argument of a post title, creates a new post with it and associates the post and author
     post_title = Post.new(post_title)
     post_title.author = self
   end
 
   def self.post_count
-    # Author .post_count is a class method that returns the total number of posts associated to all existing authors
     Post.all.select { self }.count
   end
 
