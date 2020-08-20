7d8042d79eea09ff2490513eb875cc871ef1f783
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-20 21:57:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,8 +9,17 @@ class Artist
     @songs
   end
 
-  def add_song
-    
+  def add_song(new_song)
+    # takes in an argument of a song and associates that song with the artist by telling the song that it belongs to that artist
   end
-  
+
+  def add_song_by_name(new_song)
+    # takes in an argument of a song name, creates a new song with it and associates the song and artist (FAILED - 3)
+  end
+
+  def self.song_count
+    # is a class method that returns the total number of songs associated to all existing artists (FAILED - 4)
+
+  end
+
 end
