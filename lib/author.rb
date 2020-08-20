20e051aeeaf57ea460d7ae3a664d76fa4ea79902
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-20 22:25:07 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,4 +9,8 @@ class Author
     Post.all.select { self }
   end
 
+  def add_post
+    # Author #add_post takes in an argument of a post and associates that post with the author by telling the post that it belongs to that author
+  end
+
 end
