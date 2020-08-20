4f1d8d1068b7daccfd32bf22711f02ba52b97a74
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-20 22:10:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,7 +22,7 @@ class Artist
 
   def self.song_count
     # is a class method that returns the total number of songs associated to all existing artists (FAILED - 4)
-    Song.all
+    Song.all { self }
   end
 
 end
